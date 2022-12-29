0f8470bc7275b991a34fc850f77789a13f8b348b
webnur <mdnuruli291@gmai.com>
2022-12-29 08:53:12 +0600

    basic layout and home page design

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Navbar from '../Pages/Shared/Navbar/Navbar';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../Pages/Shared/Navbar/Navbar";
+import LeftSidebar from "../Pages/Shared/Sidebars/LeftSidebar";
+import RightSidebar from "../Pages/Shared/Sidebars/RightSidebar";
 
 const Layout = () => {
-    return (
-        <div>
-            <Navbar></Navbar>
-            <Outlet></Outlet>
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className="grid grid-cols-4 gap-4 container mx-auto">
+        <div className="col-1">
+          <LeftSidebar></LeftSidebar>
         </div>
-    );
+
+        <div className="col-span-2 mx-auto">
+          <Outlet></Outlet>
+        </div>
+
+        <div className=" col-1">
+        <RightSidebar></RightSidebar>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Layout;
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,33 +1,34 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../Layout/Layout";
-import Home from '../Pages/Home/Home';
-import Login from '../Pages/Login/Login';
-import SignUp from '../Pages/SignUp/SignUp';
-import Media from '../Pages/Media/Media';
+import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
+import SignUp from "../Pages/SignUp/SignUp";
+import Media from "../Pages/Media/Media";
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Layout></Layout>,
-        children: [
-            {
-                path: '/',
-                element: <Home></Home>
-            },
-            {
-                path: '/login',
-                element: <Login></Login>
-            },
-            {
-                path: '/signup',
-                element: <SignUp></SignUp>
-            },
-            {
-                path: '/media',
-                element: <Media></Media>
-            }
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/media",
+        element: <Media></Media>,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login></Login>,
+  },
+    
+  {
+    path: "/signup",
+    element: <SignUp></SignUp>,
+  },
+]);
 
 export default router;
